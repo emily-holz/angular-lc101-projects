6ca0f06608a8f4ef96c2f548038e63e6e19c7327
Emily Holzman <emily.r.holzman@gmail.com>
2022-06-27 16:19:09 -0400

    finished part 1 of exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Best Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe5hbK4zehLGbKdtTDrC2Y5MaHFaUEZMDxUg&usqp=CAU';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfJY3Xhu411o7qXDQXeGcdnF0mmut811342BLIa2QtK7xsXx3qEuxO4LufwEHPGt-oJ_k&usqp=CAU';
 
   constructor() { }
 
